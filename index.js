144c291975b15de23caf03707a7ee78933f7ae45
anwarkvzk <anvarvzk521@gmail.com>
2024-05-20 20:38:03 +0530

    cors error bug fixed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
@@ -24,6 +25,7 @@ const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
